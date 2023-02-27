7b287de6c2d34c8f9e5321bbc7a4b204c989cd1a
DoanCongQui <congt4644@gmail.com>
2023-02-27 16:00:13 +0700

    Code C++

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
 using namespace std;
+
 int main ()
 {
-    cout <<"Hello wordl";
+    int n;
+    cout << "n = ";
+    cin >> n;    
+    for ( int i = 0; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+    
     return 0;
 }
