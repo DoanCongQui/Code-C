de75f947ac40e483368f800d66b4987a90e790c4
DoanCongQui <congt4644@gmail.com>
2023-03-05 13:44:40 +0700

    update

--- .ccls-cache/@home@dq@Documents@Code-C/main.cpp
+++ .ccls-cache/@home@dq@Documents@Code-C/main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main ()
-{
-    int n;
-    cout << "n = ";
-    cin >> n;    
-    for ( int i = 0; i <= n; i++)
-    {
-        cout << i << endl;
-    }   
-    return 0;
+int main (){
+  cout << "Hello Wordl";
+  return 0;
 }
